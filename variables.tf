cb4f85e8537816bd7384b51cc412884ff34e835d
Luis pereira <luis.pereira75@gmail.com>
2025-07-23 15:05:47 +0100

    Add workspace tag and make variables required1

--- variables.tf
+++ variables.tf
@@ -32,5 +32,3 @@ variable "public_subnets" {
   type        = map(string)
   description = "(Optional) Map of public subnets to create with CIDR blocks. Key will be used as subnet name with prefix."
   }
-}
-
